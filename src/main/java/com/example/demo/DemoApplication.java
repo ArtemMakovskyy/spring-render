f8ee8d51866ea1345c54697edcff5afcaded02e7
Artem Makovskyy <photomak79@gmail.com>
2024-02-17 13:49:40 +0200

    add ver maven

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class DemoApplication {
 //	https://www.youtube.com/watch?v=g4kQ3ELo49Y
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		run(DemoApplication.class, args);
 	}
 
 }
