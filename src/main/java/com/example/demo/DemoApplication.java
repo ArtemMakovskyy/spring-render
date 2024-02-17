46628c81e0706c41bf2daca96f76bc5f9b09207d
Artem Makovskyy <photomak79@gmail.com>
2024-02-17 12:50:18 +0200

    test3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//	https://www.youtube.com/watch?v=g4kQ3ELo49Y
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
